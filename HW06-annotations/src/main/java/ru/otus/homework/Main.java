c000212dc2eccdb2ab3eeb942eda4d0571ef44df
ParapetuumMobile <Vladimir.rte@gmail.com>
2025-07-25 01:06:10 +0300

    фикс коммента

--- HW06-annotations/src/main/java/ru/otus/homework/Main.java
+++ HW06-annotations/src/main/java/ru/otus/homework/Main.java
@@ -4,14 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Демонстрация простого тестового фреймворка ===\n");
 
-        System.out.println("----------- ");
-        // System.out.println("----------- Запускаем тесты для ExampleTest");
-        System.out.println("----------- ");
-        // TestRunner.runTests("ru.otus.homework.ExampleTest");
-        System.out.println("----------- ");
-        System.out.println("----------- Запускаем тесты для CalculatorTest");
-        System.out.println("----------- ");
-        TestRunner.runTests("ru.otus.homework.CalculatorTest");
+        System.out.println("Запускаем тесты для ExampleTest");
+        TestRunner.runTests("ru.otus.homework.ExampleTest");
+
 
         System.out.println("\n=== Демонстрация завершена ===");
     }
